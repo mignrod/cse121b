0806ef61a5e489651c2f4f7a849932478f4412dc
mignrod <mignrod@gmail.com>
2024-01-26 18:12:27 -0400

    finish

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -67,8 +67,6 @@ document.getElementById("getTotal").addEventListener("click", function(){
 
 });
 
-
-
 /* ARRAY METHODS - Functional Programming */
 /* Output Source Array */
 let arr = [1,2,3,4,5,6,7,8,9,10,11,12,13];
