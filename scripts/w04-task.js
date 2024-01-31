1117369eb883e84684044c174f45f195f74dc0bd
mignrod <mignrod@gmail.com>
2024-01-31 12:36:52 -0400

    finished

--- scripts/w04-task.js
+++ scripts/w04-task.js
@@ -29,26 +29,49 @@ let myProfile = {
 /* Populate Profile Object with placesLive objects */
 myProfile.placesLived.push(
     {
-        place: "Barquisimeto, Lara",
+        place: "Barquisimeto, Venezuela",
         length: "40 years"
+    },
+    {
+        place: "Caracas, Venezuela",
+        length: "1 year"
     }
 );
 
-
 /* DOM Manipulation - Output */
-document.querySelector("#name") = myProfile.name;
 
 /* Name */
+document.querySelector("#name").textContent = myProfile.name;
 
 /* Photo with attributes */
-
+document.querySelector("#photo").src = myProfile.photo;
+document.querySelector("#photo").alt = `Profile image of ${myProfile.name}`;
 
 /* Favorite Foods List*/
-
+myProfile.favoriteFoods.forEach(food => {
+    let liElement = document.createElement("li");
+    liElement.textContent = food;
+    document.querySelector("#favorite-foods").appendChild(liElement);
+    
+});
 
 /* Hobbies List */
-
+myProfile.hobbies.forEach(hobbie => {
+    let liElementHobbie = document.createElement("li");
+    liElementHobbie.textContent = hobbie;
+    document.querySelector("#hobbies").appendChild(liElementHobbie);
+    
+});
 
 /* Places Lived DataList */
+myProfile.placesLived.forEach(placeLived => {
+    let placeElement = document.createElement("dt");
+    let lenghtElement = document.createElement("dd");
 
+    placeElement.textContent = placeLived.place;
+    lenghtElement.textContent = placeLived.length
 
+    document.querySelector("#places-lived").appendChild(placeElement);
+    document.querySelector("#places-lived").appendChild(lenghtElement);
+    
+});
