cb89f8b96baa41629665421be7c53e1974564c1e
mignrod <mignrod@gmail.com>
2024-01-31 11:44:27 -0400

    Saving starting files

---
+++ scripts/w04-task.js
@@ -0,0 +1,54 @@
+/* LESSON 3 - Programming Tasks */
+
+/* Profile Object  */
+let myProfile = {
+    name: "Miguelangel Rodriguez",
+    photo: "images/me.jpeg",
+    favoriteFoods: [
+        "Hallacas",
+        "Pabellon", 
+        "Pizza",
+        "Pasticho",
+        "Hamburgers", 
+        "Grilled Chicken"
+    ],
+    hobbies: [
+        "Play Soccer",
+        "Read",
+        "Watch TV Series",
+        "Code",
+        "Listen to Music"
+    ],
+    placesLived: [
+
+    ],
+
+};
+
+
+/* Populate Profile Object with placesLive objects */
+myProfile.placesLived.push(
+    {
+        place: "Barquisimeto, Lara",
+        length: "40 years"
+    }
+);
+
+
+/* DOM Manipulation - Output */
+document.querySelector("#name") = myProfile.name;
+
+/* Name */
+
+/* Photo with attributes */
+
+
+/* Favorite Foods List*/
+
+
+/* Hobbies List */
+
+
+/* Places Lived DataList */
+
+
