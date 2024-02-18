fe194f23d776a7c1715e4379aecba8926b509f8d
mignrod <mignrod@gmail.com>
2024-02-18 02:35:50 -0400

    just wait for style

--- scripts/project.js
+++ scripts/project.js
@@ -1,6 +1,8 @@
 /* W06: Project */
 
 const phonesElement  = document.querySelector('#phones');
+const phonesSpecs = document.querySelector('#specs');
+
 let phoneList = [];
 
 
@@ -12,17 +14,18 @@ const getPhones = async () => {
         sortedPhones(phoneList);
 
     }
-
-};
+}
 
 /* Funtion to displayed cards of phones and populate the grid */
 const displayedPhones = (phones) => {
     phones.forEach(phone => {
+        /* Elements for cellphones cards */
         let article = document.createElement('article');
         let h3 = document.createElement('h3');
         let img = document.createElement('img');
         let details = document.createElement('button');
         details.textContent = 'Check Specs';
+        details.id = (phone.model);
 
         img.src = (phone.imageurl);
         img.alt = (`Image ${phone.brand} ${phone.model}`);
@@ -35,16 +38,65 @@ const displayedPhones = (phones) => {
         article.appendChild(details);
 
         phonesElement.appendChild(article);
+        
+        details.addEventListener('click', () => {
+            /* Elements for popups specs details */
+            let articleSpecs = document.createElement('article');
 
-    });
-    
+            let h3Specs = document.createElement('h3');
+            h3Specs.textContent = `${phone.brand} ${phone.model}`;
+
+            let imgSpecs = document.createElement('img');
+            imgSpecs.src = (phone.imageurl);
+            imgSpecs.alt = (`Image ${phone.brand} ${phone.model}`);
+
+
+            articleSpecs.appendChild(h3Specs);
+            articleSpecs.appendChild(imgSpecs);
+
+            phone.specs.forEach(specification => {
+                let art = document.createElement('li');
+                art.textContent = specification;
+                document.getElementsByName("articleSpecs").appendChild(art);
+
+            })
+
+            // articleSpecs.appendChild(art);
+
+
+
+            phonesSpecs.appendChild(articleSpecs);
+
+            // let openPopup = document.getElementById(details.id);
+            
+            let	overlay = document.getElementById('overlay');
+            
+            let	popup = document.getElementById('popup');
+            
+            overlay.classList.add('active');
+            popup.classList.add('active');
+            details.classList.add('active');
+            // let	closePopup = document.getElementById('btn-cerrar-popup');
+            
+            
+            // closePopup.addEventListener('click', function(e){
+            //     e.preventDefault();
+            //     overlay.classList.remove('active');
+            //     popup.classList.remove('active');
+            // });
+        });
+
+
+
+});
+
 }
 
 /* reset Function */
 const reset = () => {
-    templesElement.innerHTML = '';
+    phonesElement.innerHTML = '';
 
-}
+};
 
 /* filterPhones Function before sorted, call reset() function to clear lists every filter choice */
 const filterPhones = (phones) => {
@@ -94,7 +146,36 @@ const sortedPhones = (phones) => {
 
 }
 
+/* Function to show specs of the selected phone */
+// const showSpecs = (phones) => {
+//     let openPopup = phones.document.getElementById(phone.model);
 
+//     let	overlay = document.getElementById('overlay');
+
+//     let	popup = document.getElementById('popup');
+
+//     let	closePopup = document.getElementById('btn-cerrar-popup');
+
+// openPopup.addEventListener('click', () => {
+// 	overlay.classList.add('active');
+// 	popup.classList.add('active');
+// });
+
+// closePopup.addEventListener('click', function(e){
+// 	e.preventDefault();
+// 	overlay.classList.remove('active');
+// 	popup.classList.remove('active');
+// });
+// }
+
+
+
+
+/* Add the event listeners */ 
+document.querySelector('#filtered-brand').addEventListener('change', () => {
+    filterPhones(phoneList);
+
+});
 
 getPhones();
 
